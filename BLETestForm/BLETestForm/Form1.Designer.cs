58701473cfcb6b200e3a59d426eec6553d87a494
Sasha <aaaholter@gmail.com>
2019-01-18 17:31:19 +0200

    3 list boxes = OK

--- BLETestForm/BLETestForm/Form1.Designer.cs
+++ BLETestForm/BLETestForm/Form1.Designer.cs
@@ -31,6 +31,11 @@
 			this.Connect = new System.Windows.Forms.Button();
 			this.DeviceList = new System.Windows.Forms.ListBox();
 			this.Disconnect = new System.Windows.Forms.Button();
+			this.Services = new System.Windows.Forms.ListBox();
+			this.Properties = new System.Windows.Forms.ListBox();
+			this.Read = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.Write = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// Connect
@@ -48,12 +53,13 @@
 			this.DeviceList.FormattingEnabled = true;
 			this.DeviceList.Location = new System.Drawing.Point(212, 31);
 			this.DeviceList.Name = "DeviceList";
-			this.DeviceList.Size = new System.Drawing.Size(224, 173);
+			this.DeviceList.ScrollAlwaysVisible = true;
+			this.DeviceList.Size = new System.Drawing.Size(307, 69);
 			this.DeviceList.TabIndex = 1;
 			// 
 			// Disconnect
 			// 
-			this.Disconnect.Location = new System.Drawing.Point(26, 76);
+			this.Disconnect.Location = new System.Drawing.Point(26, 61);
 			this.Disconnect.Name = "Disconnect";
 			this.Disconnect.Size = new System.Drawing.Size(139, 23);
 			this.Disconnect.TabIndex = 2;
@@ -61,11 +67,60 @@
 			this.Disconnect.UseVisualStyleBackColor = true;
 			this.Disconnect.Click += new System.EventHandler(this.Disconnect_Click);
 			// 
+			// Services
+			// 
+			this.Services.FormattingEnabled = true;
+			this.Services.Location = new System.Drawing.Point(212, 106);
+			this.Services.Name = "Services";
+			this.Services.ScrollAlwaysVisible = true;
+			this.Services.Size = new System.Drawing.Size(307, 69);
+			this.Services.TabIndex = 3;
+			this.Services.SelectedIndexChanged += new System.EventHandler(this.Services_SelectedIndexChanged);
+			// 
+			// Properties
+			// 
+			this.Properties.FormattingEnabled = true;
+			this.Properties.Location = new System.Drawing.Point(212, 181);
+			this.Properties.Name = "Properties";
+			this.Properties.ScrollAlwaysVisible = true;
+			this.Properties.Size = new System.Drawing.Size(307, 82);
+			this.Properties.TabIndex = 4;
+			// 
+			// Read
+			// 
+			this.Read.Location = new System.Drawing.Point(26, 178);
+			this.Read.Name = "Read";
+			this.Read.Size = new System.Drawing.Size(139, 23);
+			this.Read.TabIndex = 5;
+			this.Read.Text = "Read";
+			this.Read.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(26, 207);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(139, 20);
+			this.textBox1.TabIndex = 6;
+			// 
+			// Write
+			// 
+			this.Write.Location = new System.Drawing.Point(26, 237);
+			this.Write.Name = "Write";
+			this.Write.Size = new System.Drawing.Size(139, 23);
+			this.Write.TabIndex = 7;
+			this.Write.Text = "Write";
+			this.Write.UseVisualStyleBackColor = true;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(457, 280);
+			this.ClientSize = new System.Drawing.Size(540, 348);
+			this.Controls.Add(this.Write);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.Read);
+			this.Controls.Add(this.Properties);
+			this.Controls.Add(this.Services);
 			this.Controls.Add(this.Disconnect);
 			this.Controls.Add(this.DeviceList);
 			this.Controls.Add(this.Connect);
@@ -73,6 +128,7 @@
 			this.Text = "Form1";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -81,6 +137,11 @@
 		private System.Windows.Forms.Button Connect;
 		private System.Windows.Forms.ListBox DeviceList;
 		private System.Windows.Forms.Button Disconnect;
+		private System.Windows.Forms.ListBox Services;
+		private System.Windows.Forms.ListBox Properties;
+		private System.Windows.Forms.Button Read;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button Write;
 	}
 }
 
--- BLETestForm/BLETestForm/Form1.cs
+++ BLETestForm/BLETestForm/Form1.cs
@@ -56,7 +56,6 @@ namespace BLETestForm
 					DeviceList.Items.Add(deviceInterface.Name);
 					count++;
 				}
-				int stop = 0;
 			}
 		}
 		////////////////////////////////////////////////////////////////////////////////////////////
@@ -81,11 +80,45 @@ namespace BLETestForm
 			btDevice = new BleDevice();
 			btDevice.StartDiscovery();
 		}
-////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////////////////////
+
+		private ObservableCollection<BluetoothLEAttributeDisplay> ServiceCollection = new ObservableCollection<BluetoothLEAttributeDisplay>();
+		private ObservableCollection<BluetoothLEAttributeDisplay> CharacteristicCollection = new ObservableCollection<BluetoothLEAttributeDisplay>();
+
 
 		private async void connect(DeviceInformation deviceInfo)
 		{
 			bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(deviceInfo.Id);
+
+			if (bluetoothLeDevice == null) return;
+
+			GattDeviceServicesResult result = await bluetoothLeDevice.GetGattServicesAsync();
+
+
+			if (result.Status == GattCommunicationStatus.Success)
+			{
+				Services.Items.Clear();
+				Properties.Items.Clear();
+				var services = result.Services;
+
+				int n = 0;
+				foreach (var service in services)
+				{
+					ServiceCollection.Add(new BluetoothLEAttributeDisplay(service));
+					Console.WriteLine("     Service:   " + ServiceCollection[n].Name);
+					Services.Items.Add(ServiceCollection[n].Name);
+					n++;
+				}
+				if (n > 0) {
+					Services.SetSelected(0, true);
+				}
+
+
+				int stop = 0;
+				// ...
+			}
+
+
 		}
 ////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -129,6 +162,7 @@ namespace BLETestForm
 		}
 		////////////////////////////////////////////////////////////////////////////////////////////
 
+
 		private void Disconnect_Click(object sender, EventArgs e)
 		{
 			if (bluetoothLeDevice != null)
@@ -138,6 +172,60 @@ namespace BLETestForm
 				Console.WriteLine("DisConnect ");
 
 			}
+
+		}
+		////////////////////////////////////////////////////////////////////////////////////////////
+
+
+		private async void Services_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int index = -1;
+			for (int x = 0; x < Services.Items.Count; x++)
+			{
+				if (Services.GetSelected(x) == true)
+				{
+					index = x;
+					break;
+				}
+			}
+			if (index < 0) return;
+
+			var attributeInfoDisp = (BluetoothLEAttributeDisplay)ServiceCollection[index];
+
+			CharacteristicCollection.Clear();
+			Properties.Items.Clear();
+
+			IReadOnlyList<GattCharacteristic> characteristics = null;
+
+			var accessStatus = await attributeInfoDisp.service.RequestAccessAsync();
+			if ((int)accessStatus == (int)DeviceAccessStatus.Allowed)
+			{
+
+				var result = await attributeInfoDisp.service.GetCharacteristicsAsync(BluetoothCacheMode.Uncached);
+				if (result.Status == GattCommunicationStatus.Success)
+				{
+					characteristics = result.Characteristics;
+				}
+				else
+				{
+					Console.WriteLine("Ошибка доступа к сервису.");
+				}
+
+
+				int n = 0;
+				foreach (GattCharacteristic c in characteristics)
+				{
+					CharacteristicCollection.Add(new BluetoothLEAttributeDisplay(c));
+					Properties.Items.Add(CharacteristicCollection[n].Name);
+					n++;
+				}
+			}
+			else
+			{
+				Console.WriteLine("DeviceAccessStatus.Allowed = false");
+			}
+
+
 
 		}
 		////////////////////////////////////////////////////////////////////////////////////////////

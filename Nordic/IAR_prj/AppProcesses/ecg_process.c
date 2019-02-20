ecdf76db903134e72df1d47e1455ecb140c8c136
Sasha <aaaholter@gmail.com>
2019-02-20 10:59:51 +0200

    Simulator ECG added

--- Nordic/IAR_prj/AppProcesses/ecg_process.c
+++ Nordic/IAR_prj/AppProcesses/ecg_process.c
@@ -21,26 +21,46 @@ host interfase
 #include "nrf_drv_timer.h"
 #include "compressor.h"
 
-const nrf_drv_timer_t TIMEOUT_TIMER_100MS = NRF_DRV_TIMER_INSTANCE(2);
+const nrf_drv_timer_t TIMER_500HZ = NRF_DRV_TIMER_INSTANCE(2);
 static bool initDone = false;
-////////////////////////////////////////////////////////////////////////////////
+static uint16_t ecgVector[MAX_CHANNELS];
+static enum { ADS1294, ADS1298 } chipId;
+static uint16_t channels;
+static uint16_t channelsOnChip[] = { 3, 8 };
 
-/*
-void processResetTimeout() {
+#define SIMULATOR
 
-	nrf_drv_timer_clear(&TIMEOUT_TIMER_100MS);
+#ifdef SIMULATOR
+#include "simulator_ecg.h"
+uint16_t simulatorIndex = 0;
+
+static void getEcgFromSimulator(){
+	uint16_t index = simulatorIndex << 3;
+	if ( simulatorIndex >= SIMULATOR_ECG_LENGTH ) {
+		// qrs pause
+		index = 0;
+	}
+	
+	for ( uint16_t i = 0; i < channels; i++ ) {
+		ecgVector[i] = simulatorEcgSignal[index++]<<1;
+	}
+	simulatorIndex++;
+	if ( simulatorIndex > SIMULATOR_ECG_PERION ) {
+		simulatorIndex = 0;
+	}
 }
+#endif // SIMULATOR
 ////////////////////////////////////////////////////////////////////////////////
-*/
+
 
 static void timeoutTimerHandler(nrf_timer_event_t event_type, void* p_context) {
-	
-	uint32_t  temp_ts = getTimestamp();
-	for (int i = 0; i < 16; i++) {
-		compressorAddRaw32(temp_ts);
-	}
 
-	NRF_LOG_INFO("no data from turbine timeout 100 ms");
+	memset(ecgVector, 0, sizeof(ecgVector));
+#ifdef SIMULATOR
+	getEcgFromSimulator();
+#endif // SIMULATOR
+	compressorAddVector((short*)ecgVector);
+
 }
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -58,14 +78,13 @@ void initTimeoutTimer(){
 	};
 
 	uint32_t err_code = NRF_SUCCESS;
-	err_code = nrfx_timer_init(&TIMEOUT_TIMER_100MS, &timer_cfg, timeoutTimerHandler);
+	err_code = nrfx_timer_init(&TIMER_500HZ, &timer_cfg, timeoutTimerHandler);
 	APP_ERROR_CHECK(err_code);
 
-	uint32_t timeMs = 500;
-	uint32_t timeTicks = nrf_drv_timer_ms_to_ticks(&TIMEOUT_TIMER_100MS, timeMs);
+	uint32_t timeTicks = 2500; //nrf_drv_timer_ms_to_ticks(&TIMEOUT_TIMER_100MS, timeMs);
 	
 	nrf_drv_timer_extended_compare(
-		 &TIMEOUT_TIMER_100MS, NRF_TIMER_CC_CHANNEL0, timeTicks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
+		 &TIMER_500HZ, NRF_TIMER_CC_CHANNEL0, timeTicks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, true);
 
 	initDone = true;
 }
@@ -74,7 +93,13 @@ void initTimeoutTimer(){
 
 void processInit() {
 	
+	chipId = ADS1294;
+	channels = channelsOnChip[chipId];
+
+	compressorInit(channels);
 	initTimeoutTimer();
+	
+
 }
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -84,8 +109,8 @@ void sensorProcessStart() {
 	if ( !initDone ) {
 		processInit();
 	}
-	nrf_drv_timer_clear(&TIMEOUT_TIMER_100MS);
-	nrf_drv_timer_enable(&TIMEOUT_TIMER_100MS);
+	nrf_drv_timer_clear(&TIMER_500HZ);
+	nrf_drv_timer_enable(&TIMER_500HZ);
 }
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -95,6 +120,6 @@ void sensorProcessStop() {
 	if ( !initDone ) { // need it in debug mode
 		initTimeoutTimer();
 	}
-	nrf_drv_timer_disable(&TIMEOUT_TIMER_100MS);
+	nrf_drv_timer_disable(&TIMER_500HZ);
 }
 ////////////////////////////////////////////////////////////////////////////////
--- Nordic/IAR_prj/AppProcesses/ecg_process.h
+++ Nordic/IAR_prj/AppProcesses/ecg_process.h
@@ -1,9 +1,11 @@
 #ifndef _ECG_PROCESS_H_
 #define _ECG_PROCESS_H_
 
+
+#define MAX_CHANNELS  8
+
 void processInit();
 void sensorProcessStart();
 void sensorProcessStop();
-//void processResetTimeout();
 
 #endif // _ECG_PROCESS_H_
